8a23229c906143717767c0d42881def8eb58dd56
brunoazca <76236184+brunoazca@users.noreply.github.com>
2023-09-28 13:40:33 +0000

    Atividade 5 (sem extras) completa

--- appdobruno/views.py
+++ appdobruno/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Sports, Routine, Activities
 from .forms import SportsForm, ActivitiesForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def home(request):
   sports= Sports.objects.all()
   routine= Routine.objects.all()
   activities= Activities.objects.all()
   return render(request, "home.html",context={"esportes":sports,"rotina":routine,"atividades":activities})
 
+@login_required
 def add_or_edit_sport(request, slug=None):
     if slug:
         sport = get_object_or_404(Sports, slug=slug)
@@ -24,7 +28,8 @@ def add_or_edit_sport(request, slug=None):
         form = SportsForm(instance=sport)
     
     return render(request, 'sports_form.html', {'form': form})
-  
+
+@login_required
 def delete_sport(request, slug):
     sport = get_object_or_404(Sports, slug=slug)
     if request.method == 'POST':
@@ -33,6 +38,7 @@ def delete_sport(request, slug):
   
     return render(request, 'delete_sports.html', {'sport': sport})
 
+@login_required
 def add_or_edit_activity(request, slug=None):
     if slug:
         activity = get_object_or_404(Activities, slug=slug)
@@ -48,7 +54,8 @@ def add_or_edit_activity(request, slug=None):
         form = ActivitiesForm(instance=activity)
     
     return render(request, 'activities_form.html', {'form': form})
-  
+
+@login_required
 def delete_activity(request, slug):
     activity = get_object_or_404(Activities, slug=slug)
     if request.method == 'POST':
@@ -57,3 +64,35 @@ def delete_activity(request, slug):
   
     return render(request, 'delete_activity.html', {'activity': activity})
 
+def create_user(request):
+  if request.method == "POST":
+    user = User.objects.create_user(
+      request.POST["username"],
+      request.POST["email"], 
+      request.POST["password"]
+    )
+    user.save()
+    return redirect("home")
+  return render(request, "register.html", context={"action": "Adicionar"})
+
+def login_user(request):
+  if request.method == "POST":
+    user = authenticate(
+      username = request.POST["username"],
+      password = request.POST["password"]
+    )
+
+    if user:
+      login(request, user)
+    else:
+      return render(request, "login.html", context={"error_msg": "Usuário não existe"})
+    print(request.user)
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated:
+      return redirect("home")
+    return render(request, "login.html", context={"error_msg": "Usuário não pode ser autenticado"})
+  return render(request, "login.html")
+
+def logout_user(request):
+  logout(request)
+  return redirect("login")
--- django_project/urls.py
+++ django_project/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from appdobruno import views
 
 urlpatterns = [
+    path('users/login/', views.login_user, name="login"),
+    path('users/logout/', views.logout_user, name="logout"),
+    path('users', views.create_user),
+    #path('users/', include('django.contrib.auth.urls')),
     path("", views.home,name="home"),
     path('admin/', admin.site.urls),
     path('sports/add/', views.add_or_edit_sport, name='add_sport'),
